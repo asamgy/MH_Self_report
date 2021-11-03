c3a490697996d03816785a6162e56f70f0b5f702
Kumar Saurav <athena.saurav@gmail.com>
2021-11-03 19:07:34 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+SPEAKER_SPEED = 150
+
+#SQL Configuration
+HOST = "localhost"
+USER = "yourusername"
+PASSWORD = "yourpassword"
+DATABASE = "databasetablename"
+
+
+ERROR = "Sorry i couldnt understand please say again."
